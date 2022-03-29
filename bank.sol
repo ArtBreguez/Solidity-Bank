d81b5ffd220a3a7035ea8b25a6a7593e410ba782
Arthur Gonçalves Breguez <98524696+ArtBreguez@users.noreply.github.com>
2022-03-29 00:21:04 -0300

    Update bank.sol
    função de empréstimo básica finalizada

--- bank.sol
+++ bank.sol
@@ -8,6 +8,8 @@ contract Bank {
         admin = payable(msg.sender);
     }
 
+    receive() external payable {}
+
     struct Clients{
         string name;
         address payable account;
@@ -17,12 +19,15 @@ contract Bank {
     }
 
     mapping(address => Clients) bank_clients;
-    mapping(uint => address payable) debt_clients;
+    mapping(address => bool) debt_clients;
+    mapping(address => uint) debt_value;
+    mapping(address => uint) amount_paid_back;
+    mapping(address => uint) amount_user_can_loan;
 
     address payable public admin;
-    address payable[] internal inDebt;
     
     function addClient(string calldata _name) public {
+        require(msg.sender != admin, "Owner can not be a client");
         Clients memory client = Clients(_name, payable(msg.sender), false, "null", "null");
         bank_clients[msg.sender] = client;
     }
@@ -42,5 +47,25 @@ contract Bank {
     function verifiedClient(address payable _address) external {
         require(msg.sender == admin, "Not the owner!");
         bank_clients[_address].isClient = true;
+        amount_user_can_loan[_address] = 1 ether;
+    }
+    function setInDebt(uint _value) internal {
+        debt_clients[msg.sender] = true;
+        debt_value[msg.sender] = _value;
+    }
+
+    function getLoan(uint _amount) public payable returns(bool){
+        require(_amount != 0, "Amount can not be zero");
+        require(msg.sender == payable(msg.sender), "Address must be payable");
+        require(bank_clients[msg.sender].isClient == true, "Must be a verified client");
+        require(debt_clients[msg.sender] == false, "Your current account is in debt");
+        require(amount_user_can_loan[msg.sender] >= _amount, "You can not loan this amout");
+        uint total_balance = address(this).balance;
+        require(total_balance > _amount, "Sorry, we don't have this money to loan");
+        uint amount_in_wei = (_amount*1000000000000000000);
+        payable(msg.sender).transfer(amount_in_wei);
+        debt_clients[msg.sender] = true;
+        debt_value[msg.sender] = _amount;
+        return true;
     }
 }
